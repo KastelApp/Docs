3a6e0b5e516b02c83be0d65df734c32c753514da
TheTeaCup <lw0482352@gmail.com>
2022-11-19 22:14:48 -0500

    generalize seo

---
+++ components/seo.js
@@ -0,0 +1,12 @@
+import { NextSeo } from 'next-seo'
+
+const SEO = ({ title, description }) => (
+    <NextSeo
+        title={title}
+        description={description}
+        openGraph={{ title, description }}
+        titleTemplate={'%s - Kastel App'}
+    />
+)
+
+export default SEO
